b5cc8b892e19b64aa598e7e6e76c91eec519abfd
Sam Achola <sam.achola@live.com>
2019-03-16 13:08:21 +0300

    Redesign service providers page. Add settings page function.

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore} from '@angular/fire/firestore';
+import { CategoryService } from 'src/app/services/categories/category.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    public categoryService: CategoryService,
+    private afFirestore: AngularFirestore,
+  ) { }
 
   ngOnInit() {
+    this.categories();
+  }
+
+  async categories () {
+    const categories = [];
+    const querySnapshot = await this.categoryService.getAllCategories();
+    querySnapshot.subscribe(docs => {
+      docs.forEach(doc => categories.push[doc.data().name]);
+    });
+
+    return categories;
   }
 
 }
